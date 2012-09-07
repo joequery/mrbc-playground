4ce2adef4e4465fe09717630320323de0cf2998d
Joseph McCullough <joseph@vertstudios.com>
2012-09-07 10:37:03 -0500

    Aesthetic changes

--- driver.c
+++ driver.c
@@ -7,34 +7,21 @@
 
 void mrb_init_mrubybin(mrb_state*);
 
-/* Print a short remark for the user */
-void print_hint(void)
-{
-  printf("mrbtest - Embeddable Ruby Test\n");
-  printf("\nThis is a very early version, please test and report errors.\n");
-  printf("Thanks :)\n\n");
-}
-
-static int
-check_error(mrb_state *mrb)
-{
+static int check_error(mrb_state *mrb){
   /* Error check */
   /* $ko_test and $kill_test should be 0 */
   mrb_value ko_test = mrb_gv_get(mrb, mrb_intern(mrb, "$ko_test"));
   mrb_value kill_test = mrb_gv_get(mrb, mrb_intern(mrb, "$kill_test"));
 
-  return FIXNUM_P(ko_test) && mrb_fixnum(ko_test) == 0 && FIXNUM_P(kill_test) && mrb_fixnum(kill_test) == 0;
+  return FIXNUM_P(ko_test) && mrb_fixnum(ko_test) == 0 && 
+         FIXNUM_P(kill_test) && mrb_fixnum(kill_test) == 0;
 }
 
-int
-main(void)
-{
+int main() {
   mrb_state *mrb;
   mrb_value return_value;
   const char *prog = "report()";
   int ret = EXIT_SUCCESS;
-
-  print_hint();
 
   /* new interpreter instance */
   mrb = mrb_open();
